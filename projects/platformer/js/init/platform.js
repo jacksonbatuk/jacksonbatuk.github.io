49437ca0686ec6efd6c59dceb9e855c65815fb2d
jacksonbatuk <jacksonbatuk0311@gmail.com>
2021-11-30 14:47:14 +0000

    small platform moved

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -37,8 +37,8 @@
         createPlatform(0, game.world.height - 32, 3, 2);    // DO NOT DELETE
 
         // example:
-        createPlatform(400, 250);
-        createPlatform(170, 460, 0.5);
+        createPlatform(400, 250);//long platform
+        createPlatform(170, 490, 0.5);//small platform
         
         // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
         ////////////////////////////////////////////////////////////////////////
