de39201e07736286c1248f39359a9d40c91a2d17
jacksonbatuk <jacksonbatuk0311@gmail.com>
2022-02-14 14:29:44 +0000

    added bower

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,14 +20,25 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+        var circle;
+        var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle() {
+            circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);//draws the circle
+            physikz.addRandomVelocity(circle, canvas);//adds a random velocity
+            view.addChild(circle);
+            circles.push(circle);//pushes single circle to array
+        }
+
 
         // TODO 3 / 8 : Call the drawCircle() function 
-
-
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
         ////////////////////////////////////////////////////////////
@@ -39,10 +50,18 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
-            
+            physikz.updatePosition( circle[0] );
+            physikz.updatePosition( circle[1] );
+            physikz.updatePosition( circle[2] );
+            physikz.updatePosition( circle[3] );
+            physikz.updatePosition( circle[4] );
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
+           game.checkCirclePosition( circle[0]);
+           game.checkCirclePosition( circle[1]);
+           game.checkCirclePosition( circle[2]);
+           game.checkCirclePosition( circle[3]);
+           game.checkCirclePosition( circle[4]);
+
 
             // TODO 9 : Iterate over the array
            
